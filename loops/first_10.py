311aed5fa9774908f308043ad159ae9592d41db7
yosifbostandzhiev <YOSIF.PYTHON@GMAIL.COM>
2021-09-05 12:25:40 +0300

    :

--- loops/first_10.py
+++ loops/first_10.py
@@ -1,18 +1,18 @@
-# 1.    Write a program that inputs a positive integer N and outputs the factorial of 2N
-# 2.    Write a program that counts and outputs the number of digits in Nth Fibonacci number
-# •    Example:
+# 1Write a program that inputs a positive integer N and outputs the factorial of 2N
+# 2.Write a program that counts and outputs the number of digits in Nth Fibonacci number
+# •Example:
 # Please input a positive integer: 13
 # Number of digits in F5 is: 3
-# 3.    Write a program that inputs three real numbers and outputs whether they can be sides of a triangle
-# 4.    Write a program that calculates and outputs the first N even Fibonacci numbers, separated
+# 3.Write a program that inputs three real numbers and outputs whether they can be sides of a triangle
+# 4.Write a program that calculates and outputs the first N even Fibonacci numbers, separated
 # by comma and space. N is entered from the standard input.
-# 5.    Write a program that calculates and outputs the sum of the digits of N factorial (N!).
-# 6. Write a program that checks and prints whether two rectangles overlap.The rectangles’
+# 5.Write a program that calculates and outputs the sum of the digits of N factorial (N!).
+# 6.Write a program that checks and prints whether two rectangles overlap.The rectangles’
 # sides are parallel to x and y axes.Each rectangle is determined by the coordinates of its
 # upper - left and lower - right corner.Those coordinates need to be input(you may use integer numbers).
-#  Exampleoutput: Rectangles overlap
+# Example output: Rectangles overlap
 # 7.Write a program that outputs the following triangle(height=11):.
-# Image of rectangle: https://cdn.discordapp.com/attachments/876821589923102740/883949940374638652/unknown.png
+# Image of rectangle: https://ibb.co/B2HgfVL
 # 8.The same triangle as previous, but having height and symbol input by the user.
 # 9.Write a program that outputs the first N natural numbers in a rectangle with a given width (W).
 # The width is the number of symbols ( not numbers) in a line! Numbers are printed in
@@ -20,4 +20,3 @@
 # sequence continues on the next line, etc.No spaces.The text on the console shall look as a
 # rectangular block (the last line may be incomplete).N and W are entered by the user.
 # 10.Write a program that inputs an integer and checks whether it is a prime number.
-
--- loops/prob7.py
+++ loops/prob7.py
@@ -1,3 +1,2 @@
 # 7.Write a program that outputs the following triangle(height=11):.
-# Image of rectangle: https://cdn.discordapp.com/attachments/876821589923102740/883949940374638652/unknown.png
-
+# Image of rectangle: https://ibb.co/B2HgfVL
